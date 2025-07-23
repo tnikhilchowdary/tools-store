b8de13331f8b2a4e30fe14a0b1174b1761a5cac7
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-07-23 16:37:53 -0500

    Update App.js

--- tools-store/src/App.js
+++ tools-store/src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>Welocme to the tools store</h1>
+      <h1>Welcome to the store</h1>
+      <p>Wolrds best Tool Store</p>
     </div>
   );
 }
